ccc9237677bf9d7ce28d58e2ce5578df02eff0ae
Nuwan94 <nuwansalawatta@gmail.com>
2019-03-27 03:30:00 +0530

    1.103

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 var app = angular.module("myCV", ["ngRoute"]);
 
 app.config(function ($routeProvider) {
@@ -16,6 +15,25 @@ app.config(function ($routeProvider) {
 			templateUrl: "contact.html"
 		})
 		.otherwise({
-			template : "<h1>404</h1><p>Not Found</p>"
+			template: "<h1>404</h1><p>Not Found</p>"
 		});
 });
+
+app.controller('projectCtrl', function ($scope, $http) {
+	$scope.isLoaded = true;
+	$http.get("https://api.github.com/users/nuwan94/repos?page=1&per_page=100")
+		.then(function (response) {
+			$scope.isLoaded = false;
+			$scope.projects = response.data;
+		});
+});
+
+
+
+// app.controller('projectCtrl', function ($scope, $http) {
+// 	var url = "https://api.github.com/users/nuwan94/repos?page=1&per_page=100";
+
+// 	$http.get(url).success(function (data) {
+// 		$scope.projects = data;
+// 	});
+// });
